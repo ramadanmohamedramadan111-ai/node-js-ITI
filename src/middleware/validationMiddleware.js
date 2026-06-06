3d789b94fbe88b1247a0fc8594333ee28c4af153
ramadanmohamedramadan111-ai <ramadan.mohamed.ramadan111@gmail.com>
2026-06-07 00:19:50 +0300

    two

--- src/middleware/validationMiddleware.js
+++ src/middleware/validationMiddleware.js
@@ -1,52 +1,23 @@
 const ApiError = require("../errors/apiError");
 
-const validate = (schema) => (req, res, next) => {
-  if (schema.params) {
-    const { error, value } = schema.params.validate(req.params, {
-      abortEarly: false,
-      stripUnknown: true,
-    });
+const validate = (schema) => {
+  return async (req, res, next) => {
+    try {
+      for (const key of Object.keys(schema)) {
+        if (!schema[key]) continue;
+        const value = await schema[key].validateAsync(req[key]);
+        req[key] = value;
+      }
 
-    if (error) {
+      next();
+    } catch (err) {
       return next(
-        ApiError.BadRequest(error.details.map((detail) => detail.message).join(", "))
+        ApiError.BadRequest(
+          err && err.details && err.details[0] ? err.details[0].message : err.message
+        )
       );
     }
-
-    req.params = value;
-  }
-
-  if (schema.query) {
-    const { error, value } = schema.query.validate(req.query, {
-      abortEarly: false,
-      stripUnknown: true,
-    });
-
-    if (error) {
-      return next(
-        ApiError.BadRequest(error.details.map((detail) => detail.message).join(", "))
-      );
-    }
-
-    req.query = value;
-  }
-
-  if (schema.body) {
-    const { error, value } = schema.body.validate(req.body, {
-      abortEarly: false,
-      stripUnknown: true,
-    });
-
-    if (error) {
-      return next(
-        ApiError.BadRequest(error.details.map((detail) => detail.message).join(", "))
-      );
-    }
-
-    req.body = value;
-  }
-
-  next();
+  };
 };
 
 module.exports = {
